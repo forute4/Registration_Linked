5a0c690baa874b78fe1eae7beceb20273b2f284a
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2024-02-11 15:01:58 +0000

    生徒のステータス作成

--- app/controllers/admin/subscriptions_controller.rb
+++ app/controllers/admin/subscriptions_controller.rb
@@ -1,6 +1,6 @@
 class Admin::SubscriptionsController < ApplicationController
   def index
     @students= Student.all
-    @trial_sutdents= Student.where(student_status:trial_student)
+    @trial_sutdents= Student.where(student_status: trial_student)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -7,6 +7,10 @@ class Student < ApplicationRecord
   
   enum student_status: {trial_student: 1, internal_student: 2, leaving_student: 3}
   enum situation_status: {A: 1, B: 2, C: 3}
-  enum grade: {grade_5:1, grade_6:2, grade_1:3, grade_2:4, grade_3:5}
+  enum grade: {grade_5: 1, grade_6: 2, grade_1: 3, grade_2: 4, grade_3: 5}
+  
+  def full_name
+    self.last_name + "" + self.first_name
+  end
   
 end
